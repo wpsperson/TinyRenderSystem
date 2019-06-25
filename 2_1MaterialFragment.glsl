5b47072fdf02bf9bfee773dbb4df0fdd66b2d910
wpsperson <wpsperson@163.com>
2019-06-25 22:25:05 +0800

    add shader filese

---
+++ 2_1MaterialFragment.glsl
@@ -0,0 +1,48 @@
+//²ÄÖÊÆ¬¶Î×ÅÉ«Æ÷
+#version 330 core
+out vec4 FragColor;
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 viewPos;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+
+
+
+void main()
+{
+    //1 ambient
+    vec3 ambient = light.ambient * material.ambient;
+
+    //2 diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+
+    ////3 specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
+
+    vec3 result = ambient + diffuse + specular;//
+    FragColor = vec4(result, 1.0);
+
+
+}
