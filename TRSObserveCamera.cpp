3c4fff13ec3c5a47242e7a517a3e143076aab132
wpsperson <wpsperson@163.com>
2021-08-28 21:21:03 +0800

    finish TRSObserveCamera

--- TRSObserveCamera.cpp
+++ TRSObserveCamera.cpp
@@ -8,15 +8,13 @@ TRSObserveCamera::TRSObserveCamera(GLFWwindow* pWindow)
     m_leftMousePressed = false;
     m_middleMousePressed = false;
     m_modifier = 0;
-
     //pitch yaw 角度初值的含义是通过满足 m_front = glm::vec3(0, 0, -1.0f)反算出来的
     m_fPitch = 0.0f;
     m_fYaw = -90.0f;
-
-    m_fLastX = DefaultWindowWidth / 2;
-    m_fLastY = DefaultWindowHeight / 2;
-
-    m_fMouseSensity = 0.05f;
+    m_fLastX = 0;
+    m_fLastY = 0;
+    m_fMouseSensity = 0.15f;
+    m_facialPt = glm::vec3(0, 0, 0);
 }
 
 TRSObserveCamera::~TRSObserveCamera()
@@ -39,7 +37,7 @@ void TRSObserveCamera::mouseMoveCallBack(GLFWwindow* pWindow, double xpos, doubl
         m_fLastX = xpos;
         m_fLastY = ypos;
         m_fYaw += m_fMouseSensity * xOffset;
-        m_fPitch += m_fMouseSensity * yOffset * 5;
+        m_fPitch += m_fMouseSensity * yOffset;
         if (m_fPitch > 89.0f)
         {
             m_fPitch = 89.0f;
@@ -48,11 +46,13 @@ void TRSObserveCamera::mouseMoveCallBack(GLFWwindow* pWindow, double xpos, doubl
         {
             m_fPitch = -89.0f;
         }
+        float origDist = glm::distance(m_pos, m_facialPt);
 
         m_front.y = sin(glm::radians(m_fPitch));
         m_front.x = cos(glm::radians(m_fPitch)) * cos(glm::radians(m_fYaw));
         m_front.z = cos(glm::radians(m_fPitch)) * sin(glm::radians(m_fYaw));
         m_front = glm::normalize(m_front);
+        m_pos = m_facialPt - origDist * m_front;
         m_right = glm::cross(m_front, glm::vec3(0, 1, 0));
         m_right = glm::normalize(m_right);
         m_up = glm::cross(m_right, m_front);
@@ -70,7 +70,7 @@ void TRSObserveCamera::mouseMoveCallBack(GLFWwindow* pWindow, double xpos, doubl
         glm::vec3 delta = m_up * float(yOffset) + m_right * float(xOffset);
         delta = delta*0.001f;
         m_pos -= delta;
-        m_front -= delta;
+        m_facialPt -= delta;
         return;
     }
 }
