9878ab19e8c699e76611f73ddd881a6c4a194f8a
wpsperson <wpsperson@163.com>
2024-08-06 16:47:45 +0800

    application support high dpi scale.

--- TRSExamples/TRSApplication/main.cpp
+++ TRSExamples/TRSApplication/main.cpp
@@ -4,6 +4,8 @@
 
 int main(int argc, char* argv[])
 {
+    // this must before QApplication constructor.
+    // QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     MainWindow mainwindow;
     mainwindow.show();
